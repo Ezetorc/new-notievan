96b4aac769250e7a28325b8d366925a8c9f221e4
Ezetorc <ezetorc@gmail.com>
2025-11-14 16:22:13 -0300

    Add analytics

--- frontend/src/pages/Article/components/AsideArticles.tsx
+++ frontend/src/pages/Article/components/AsideArticles.tsx
@@ -2,27 +2,25 @@ import { SecondaryArticle } from '../../../components/SecondaryArticle'
 import { usePaginatedArticles } from '../../../hooks/use-paginated-articles.hook'
 
 export function AsideArticles({ excludeId }: { excludeId: string }) {
-	const { articles, loading } = usePaginatedArticles({
-		type: 'random',
-		excludeId
-	})
+  const { articles, loading } = usePaginatedArticles({
+    type: 'random',
+    excludeId
+  })
 
-	if (loading || !articles.length) {
-		return (
-			<aside className='flex flex-col gap-y-5'>
-				<SecondaryArticle />
-				<SecondaryArticle />
-				<SecondaryArticle />
-				<SecondaryArticle />
-			</aside>
-		)
-	}
+  if (loading || !articles.length) {
+    return (
+      <aside className='flex flex-col gap-y-5'>
+        <SecondaryArticle />
+        <SecondaryArticle />
+      </aside>
+    )
+  }
 
-	return (
-		<aside className='flex flex-col gap-y-5'>
-			{articles.map((article) => (
-				<SecondaryArticle key={article.id} article={article} />
-			))}
-		</aside>
-	)
+  return (
+    <aside className='flex flex-col gap-y-5'>
+      {articles.map((article) => (
+        <SecondaryArticle key={article.id} article={article} />
+      ))}
+    </aside>
+  )
 }
