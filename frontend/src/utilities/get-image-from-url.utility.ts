b664dc63c2e50d974772b8e431070921d91db2af
Ezetorc <ezetorc@gmail.com>
2025-11-15 13:21:13 -0300

    Remove image utility functions

--- frontend/src/utilities/get-image-from-url.utility.ts
+++
@@ -1,9 +0,0 @@
-export function getImageFromUrl(url: string): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
-    const image = new Image()
-    image.crossOrigin = 'anonymous'
-    image.src = url
-    image.onload = () => resolve(image)
-    image.onerror = error => reject(error)
-  })
-}
