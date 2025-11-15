053393ccf6ae3c620bd91a96217929849fa87cce
Ezetorc <ezetorc@gmail.com>
2025-11-15 11:36:49 -0300

    Remove ImageCropper elements

--- frontend/src/pages/CreateArticle/components/CreateArticlePage.tsx
+++ frontend/src/pages/CreateArticle/components/CreateArticlePage.tsx
@@ -18,7 +18,6 @@ export default function CreateArticlePage() {
   const [, setLocation] = useLocation()
   const queryClient = useQueryClient()
   const [isLoading, setIsLoading] = useState(false)
-  // const [isModalOpen, setIsModalOpen] = useState<boolean>(true)
 
   const onSuccess = async (data: CreateArticleFormData) => {
     setIsLoading(true)
@@ -42,77 +41,57 @@ export default function CreateArticlePage() {
     subtitle: ''
   })
 
-  // const onImageSelected = (value: string | File) => {
-  //   watch('image', value)
-
-  //   if (value instanceof File) {
-  //     setIsModalOpen(true)
-  //   }
-  // }
-
   return (
-    <>
-      {/* {
-        isModalOpen && data.image instanceof File && <ImageCropperModal
-          image={data.image}
-          onComplete={(croppedImage) => {
-            watch('image', croppedImage)
-            setIsModalOpen(false)
-          }}
-        />
-      } */}
+    <form className='flex flex-col pb-[5vw] mobile:mt-[20px] tablet:mt-[60px]'>
+      <ArticleInput
+        placeholder='Subtítulo de tu artículo...'
+        minLength={1}
+        maxLength={50}
+        className='mobile:text-[20px] tablet:text-2xl font-bold font-text mb-5'
+        onChange={(value) => watch('subtitle', value)}
+      />
 
-      <form className='flex flex-col pb-[5vw] mobile:mt-[20px] tablet:mt-[60px]'>
-        <ArticleInput
-          placeholder='Subtítulo de tu artículo...'
-          minLength={1}
-          maxLength={50}
-          className='mobile:text-[20px] tablet:text-2xl font-bold font-text mb-5'
-          onChange={(value) => watch('subtitle', value)}
-        />
+      <ArticleInput
+        placeholder='Título de tu artículo...'
+        minLength={1}
+        maxLength={50}
+        className='mobile:text-4xl tablet:text-5xl font-title'
+        onChange={(value) => watch('title', value)}
+      />
 
-        <ArticleInput
-          placeholder='Título de tu artículo...'
-          minLength={1}
-          maxLength={50}
-          className='mobile:text-4xl tablet:text-5xl font-title'
-          onChange={(value) => watch('title', value)}
-        />
+      <ArticleInput
+        className='mt-5 mobile:text-[20px] tablet:text-2xl  mb-[30px]'
+        placeholder='Descripción de tu artículo...'
+        minLength={1}
+        maxLength={50}
+        onChange={(value) => watch('description', value)}
+      />
 
-        <ArticleInput
-          className='mt-5 mobile:text-[20px] tablet:text-2xl  mb-[30px]'
-          placeholder='Descripción de tu artículo...'
-          minLength={1}
-          maxLength={50}
-          onChange={(value) => watch('description', value)}
-        />
+      <div className='flex flex-col tablet:grid gap-6 tablet:gap-x-10 w-full tablet:grid-cols-[1fr] desktop:grid-cols-[2fr_1fr]'>
+        <div className='flex flex-col gap-y-4 tablet:gap-y-6 max-w-full order-2 tablet:order-1'>
+          <MarkdownEditor
+            onChange={(value) => watch('content', value)}
+            placeholder='Contenido de tu artículo...'
+          />
 
-        <div className='flex flex-col tablet:grid gap-6 tablet:gap-x-10 w-full tablet:grid-cols-[1fr] desktop:grid-cols-[2fr_1fr]'>
-          <div className='flex flex-col gap-y-4 tablet:gap-y-6 max-w-full order-2 tablet:order-1'>
-            <MarkdownEditor
-              onChange={(value) => watch('content', value)}
-              placeholder='Contenido de tu artículo...'
-            />
-
-            {error && <ErrorMessage>{error}</ErrorMessage>}
-          </div>
-
-          <aside className='flex flex-col gap-y-5 order-1 md:order-2'>
-            <ImageInput onImageSelected={(value) => watch("image", value)} />
-          </aside>
-
-          <div className='w-full order-3'>
-            <ActionButton
-              className='w-full bg-brand-orange text-white font-bold text-xl tablet:text-3xl h-[50px] tablet:h-[70px]'
-              loading={isLoading}
-              onClick={onSubmit}
-            >
-              Crear artículo
-            </ActionButton>
-          </div>
+          {error && <ErrorMessage>{error}</ErrorMessage>}
         </div>
 
-      </form>
-    </>
+        <aside className='flex flex-col gap-y-5 order-1 md:order-2'>
+          <ImageInput onImageSelected={(value) => watch('image', value)} />
+        </aside>
+
+        <div className='w-full order-3'>
+          <ActionButton
+            className='w-full bg-brand-orange text-white font-bold text-xl tablet:text-3xl h-[50px] tablet:h-[70px]'
+            loading={isLoading}
+            onClick={onSubmit}
+          >
+            Crear artículo
+          </ActionButton>
+        </div>
+      </div>
+
+    </form>
   )
 }
